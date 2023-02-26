8a2f952067df73a14d16964ce67c9fcd24a0f3b4
skygroundwater <113610956+skygroundwater@users.noreply.github.com>
2023-02-27 00:03:36 +0300

    Delete GeneralServiceImpl.java

--- src/main/java/ru/shipownerproject/services/generalservice/GeneralServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package ru.shipownerproject.services.generalservice;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class GeneralServiceImpl implements GeneralService {
-
-
-}

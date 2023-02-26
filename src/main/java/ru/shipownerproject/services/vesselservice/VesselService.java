11d7db9b7b8482e17804cfcfc931261b9c321564
skygroundwater <113610956+skygroundwater@users.noreply.github.com>
2023-02-27 00:05:37 +0300

    Delete VesselService.java

--- src/main/java/ru/shipownerproject/services/vesselservice/VesselService.java
+++
@@ -1,9 +0,0 @@
-package ru.shipownerproject.services.vesselservice;
-
-public interface VesselService {
-    String vessel(String name);
-
-    String addNewVessel(String vesselName, String shipOwnerName, String IMO);
-
-    String removeVesselFromBase(String IMO);
-}

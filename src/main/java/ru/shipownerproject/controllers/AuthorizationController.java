7fab6c7a66d8e68de29ce8cd373df15d3d3f6ed0
Nitro <o.metelev2020@yandex.ru>
2023-03-12 22:33:18 +0300

    Have been realized:
    1. Started configure Spring Security Context
    
    ...
    
    The plans are to test code using Mockito and JUnit.

---
+++ src/main/java/ru/shipownerproject/controllers/AuthorizationController.java
@@ -0,0 +1,14 @@
+package ru.shipownerproject.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthorizationController {
+
+
+
+}
